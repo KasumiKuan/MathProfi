16a14aa7b5c086e80391dff561040777950fe704
wulf97 <alexej4703@gmail.com>
2016-04-06 10:58:36 +0300

    Added files via upload

---
+++ inklude/racional/RED_Q_Q.cpp
@@ -0,0 +1,20 @@
+/*
+	Криворучко Никита 5305
+	Модуль: RED_Q_Q
+	Назначение: Сокращение дроби
+	Зависимости: GCF_NN_N
+				 DIV_ZZ_Z
+				 ABS_Z_N
+*/
+
+struct RACIONAL RED_Q_Q(struct RACIONAL rNum) {
+	struct RACIONAL rez; // Возращаемое значение
+	struct NATURAL NOD;	// НОД чисел
+	
+	NOD = GCF_NN_N(rNum.numerator, rNum.denominator); // Нахождение НОД
+	rez.numerator = DIV_ZZ_Z(ABS_Z_N(rNum.numerator), NOD); // Сокращаем модуль числителя на НОД
+	rez.denominator = DIV_ZZ_Z(ABS_Z_N(rNum.denominator), NOD);	// Сокращаем модуль знаменателя на НОД
+	rez.token = rNum.token;	//сохраним знак
+	
+	return rez;
+}
